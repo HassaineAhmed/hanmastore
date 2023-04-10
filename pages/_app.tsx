1f130c11944cf0a74b8ef22330c4b90a181b3bc0
Hassaine Ahmed <ahmed.hassine432@gmail.com>
2023-04-10 11:03:05 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import 'next/font/styles/globals.css'
 import type { AppProps } from 'next/app'
+  import { Bebas_Neue , DM_Sans} from 'next/font/google'
+
+const bebas= Bebas_Neue({ subsets: ['latin'], weight : ["400"], variable: "--font-bebas-neue", display: "swap" })
+const dm_sans= DM_Sans({
+  variable: '--font-dm-sans',
+  display: 'swap',subsets: ['latin'], weight : ["400"] 
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={`${dm_sans.variable} ${bebas.variable}`}><Component {...pageProps} /></main>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,36 @@ module.exports = {
   ],
   theme: {
     extend: {
+       fontFamily: {
+        bebas_neue: ['var(--font-bebas-neue)'],
+        dm_sans: ['var(--font-dm-sans)'],
+      },
+      animation : { 
+        wiggle : "khtafa 1s ease  alternate", 
+        slideIn : "slideIn 1s ease",
+        slideOut : "slideOut 7s ease",
+      },
+      keyframes : {
+      slideIn  : { 
+        "0%" : { transform : "translateX(300%)"},
+        "100%" : { transform : "translateX(0)"},
+      },
+      slideOut  : { 
+        "0%" : { transform : "translateX(0)" },
+        "12%" : { transform : "translateX(-300%)", opacity : 1 },
+        "13%" : { opacity : 0},
+        "100%" : { opacity : 0},
+      },
+      khtafa : {
+        "0%" : { opacity : 1 },
+        "50%" : { opacity : 0 },
+        "100%" : { opacity : 1 },
+      }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
