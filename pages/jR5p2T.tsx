c90822e3237a76b6fed55bd5f37665f2533b07da
Hassaine Ahmed <ahmed.hassine432@gmail.com>
2023-05-18 09:30:44 +0100

    added product name

--- pages/jR5p2T.tsx
+++ pages/jR5p2T.tsx
@@ -4,6 +4,7 @@ export default function Page({ orders }) {
     const { productName, fullName, phoneNumber, secondPhoneNumber, wilaya, fullAdress, quantity, size, codePromo, price } = data;
     return (<div className="grid bg-black p-5  rounded-2xl">
       <p className="text-xl text-white" ><span className="font-bold">  name </span>  : {fullName} </p>
+      <p className="text-xl text-white" ><span className="font-bold">  product  </span>  : {productName} </p>
       <p className="text-xl text-white" ><span className="font-bold">  wilaya </span>  : {wilaya} </p>
       <p className="text-xl text-white" ><span className="font-bold">  address </span>  : {fullAdress} </p>
       <p className="text-xl text-white" ><span className="font-bold">  phoneNumber </span>  : {phoneNumber} </p>
